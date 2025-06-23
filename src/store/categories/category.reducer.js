fc5fb3e6ba96da84468b3f395b42bd771591cd63
Amanda <amandaroblesurena@gmail.com>
2025-06-23 19:29:49 +0200

    Categories context in Redux

---
+++ src/store/categories/category.reducer.js
@@ -0,0 +1,20 @@
+import { CATEGORIES_ACTION_TYPES } from './category.types';
+
+export const CATEGORY_INITIAL_STATE = {
+  categoriesMap: {},
+};
+
+export const categoriesReducer = (state = CATEGORY_INITIAL_STATE, action={}) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
+      return {
+        ...state,
+        categoriesMap: payload,
+      };
+    default:
+      return state;
+  }
+}
+//Despues de crear el reducer, debemos ponerlo en root-reducer.js
